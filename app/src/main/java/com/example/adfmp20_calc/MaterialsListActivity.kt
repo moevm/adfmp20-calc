7d5682a1721405756a79ef7b400c118bbec1b1a7
StaringGenie <danil090598@gmail.com>
2020-04-15 17:44:44 +0300

    extrang strings

--- app/src/main/java/com/example/adfmp20_calc/MaterialsListActivity.kt
+++ app/src/main/java/com/example/adfmp20_calc/MaterialsListActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -40,7 +39,7 @@ class MaterialsListActivity : AppCompatActivity() {
         val total : Double? = attributes?.getDouble("total")
         val p1: Int? = attributes?.getInt("p1")
         val p2: Int? = attributes?.getInt("p2")
-     
+
 
     }
 
